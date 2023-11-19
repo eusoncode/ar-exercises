14d489d16376ba1b7153a0d526a0967031ee3cdd
eusoncode <git@github.com:eusoncode/Kata.git>
2023-11-18 20:00:59 -0700

    Exercise 4: Loading a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+# Step 1: Create 3 new stores
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Step 2: Fetch stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# Step 3: Output name and annual revenue of each men's store
+@mens_stores.each do |store|
+  puts "#{store.name} - Annual Revenue: #{store.annual_revenue}"
+end
+
+# Step 4: Fetch stores that carry women's apparel and have annual revenue less than $1M
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+# Output name and annual revenue of each women's store
+@womens_stores.each do |store|
+  puts "#{store.name} - Annual Revenue: #{store.annual_revenue}"
+end
