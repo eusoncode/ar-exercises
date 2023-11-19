6dd52c927d4783da80becc6161757f917de2eb74
eusoncode <git@github.com:eusoncode/Kata.git>
2023-11-18 19:35:22 -0700

    Exercise 3: Delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,8 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+# Output the number of stores in the database
+puts "Number of stores: #{Store.count}"
