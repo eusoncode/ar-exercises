37bfd30757da384aecfa2d349b1fceb4e34a2e60
eusoncode <git@github.com:eusoncode/Kata.git>
2023-11-18 20:17:33 -0700

    Exercise 5: Calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Step 1: Calculate the total revenue for all stores
+total_revenue = Store.sum(:annual_revenue)
+puts "Total company revenue: $#{total_revenue}"
+
+# Step 2: Calculate the average annual revenue for all stores
+average_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue for all stores: $#{average_revenue}"
+
+# Step 3: Calculate the number of stores generating $1M or more in annual sales
+stores_count = Store.where("annual_revenue >= ?", 1000000).count
+puts "Number of stores generating $1M or more in annual sales: #{stores_count}"
