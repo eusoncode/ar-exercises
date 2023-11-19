d33565c894de7b8fd4c7de3b1a7d117a5d5a7516
eusoncode <git@github.com:eusoncode/Kata.git>
2023-11-18 22:27:07 -0700

    Exercise 7: Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,14 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask user for a store name
+puts "Enter store name:"
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name
+new_store = Store.new(name: store_name)
+unless new_store.save
+  puts "Errors occurred:"
+  new_store.errors.full_messages.each { |msg| puts msg }
+end
